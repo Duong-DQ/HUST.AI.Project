c6050d4a19c537ca2b0bd0f8cbd6039946a28fc5
DuongDQ-194747 <quangduongphp@gmail.com>
2022-12-18 03:03:31 +0700

    front-end: streamlit deploy

--- app.py
+++ app.py
@@ -1,47 +1,50 @@
-import numpy as np
-from flask import Flask, request, render_template
-import pickle
-#Create an app object using the Flask class. 
-app = Flask(__name__)
+# import numpy as np
+# from flask import Flask, request, render_template
+# import pickle
+# #Create an app object using the Flask class. 
+# app = Flask(__name__)
 
-#Load the trained model. (Pickle file)
-model = pickle.load(open('Age-Gender-Prediction\checkpoints\model.h5', 'rb'))
+# model = pickle.load(open('Age-Gender-Prediction\checkpoints\model.h5', 'rb'))
 
-#Define the route to be home. 
-#The decorator below links the relative route of the URL to the function it is decorating.
-#Here, home function is with '/', our root directory. 
-#Running the app sends us to index.html.
-#Note that render_template means it looks for the file in the templates folder. 
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
-#use the route() decorator to tell Flask what URL should trigger our function.
-@app.route('/')
-def home():
-    return render_template('index.html')
+# @app.route('/predict',methods=['POST'])
+# def predict():
 
-#You can use the methods argument of the route() decorator to handle different HTTP methods.
-#GET: A GET message is send, and the server returns data
-#POST: Used to send HTML form data to the server.
-#Add Post method to the decorator to allow for form submission. 
-#Redirect to /predict page with the output
-@app.route('/predict',methods=['POST'])
-def predict():
+#     int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
+#     features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
+#     prediction = model.predict(features)  # features Must be in the form [[a, b]]
 
-    int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
-    features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
-    prediction = model.predict(features)  # features Must be in the form [[a, b]]
+#     output = round(prediction[0], 2)
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
+#     return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
 
 
-#When the Python interpreter reads a source file, it first defines a few special variables. 
-#For now, we care about the __name__ variable.
-#If we execute our code in the main program, like in our case here, it assigns
-# __main__ as the name (__name__). 
-#So if we want to run our code right here, we can check if __name__ == __main__
-#if so, execute it here. 
-#If we import this file (module) to another file then __name__ == app (which is the name of this python file).
+# if __name__ == "__main__":
+#     app.run()
 
-if __name__ == "__main__":
-    app.run()
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+
+def main():
+    st.header("How old are you according to a Convolution Neural Network? 🥸👧")
+    st.write("Upload an image of yourself below to find out! (preferably a squared image and containing only your face)")
+    file = st.file_uploader("Upload Photo")
+    if file is not None:
+        st.image(file, width=300)
+        image = Image.open(file)
+        image = tf.image.resize(image, [224,224]) 
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        image = image / 255.0      
+        image = tf.expand_dims(image, axis=0)
+        
+        model = tf.keras.models.load_model("my_model.h5")
+        age = model.predict(image)
+        st.markdown("## You're %i years old according to our model!" %age[0][0])
+        st.write("Want to know how it works? Check out the [source code in GitHub](https://github.com/ubiratanfilho/age-gender-prediction)")
+    
+if __name__ == '__main__':
+	main()
