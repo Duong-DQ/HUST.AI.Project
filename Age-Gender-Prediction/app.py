2037319b66e7c3f58c8dd1803984c0f3aeac4be5
DuongDQ-194747 <quangduongphp@gmail.com>
2022-12-18 10:20:51 +0700

    save model & deploy

---
+++ Age-Gender-Prediction/app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+
+agemodel = tf.keras.models.load_model("checkpoints\\agemodel.h5")
+genmodel = tf.keras.models.load_model("checkpoints\\genmodel.h5")
+
+def process_and_predict(file):
+    im = Image.open(file)
+    im = im.convert("RGB")
+    width, height = im.size
+    if width == height:
+        im = im.resize((200, 200), Image.ANTIALIAS)
+    else:
+        if width > height:
+            left = width / 2 - height / 2
+            right = width / 2 + height / 2
+            top = 0
+            bottom = height
+            im = im.crop((left, top, right, bottom))
+            im = im.resize((200, 200), Image.ANTIALIAS)
+        else:
+            left = 0
+            right = width
+            top = 0
+            bottom = width
+            im = im.crop((left, top, right, bottom))
+            im = im.resize((200, 200), Image.ANTIALIAS)
+
+    ar = np.asarray(im)
+    ar = ar.astype("float32")
+    ar /= 255.0
+    ar = ar.reshape(-1, 200, 200, 3)
+
+    age = agemodel.predict(ar)
+    gender = np.round(genmodel.predict(ar))
+    if gender == 0:
+        gender = "Male"
+    elif gender == 1:
+        gender = "Female"
+
+    info = "Age: " + str(int(age)) + " - Gender: " + gender
+    st.subheader(info)
+    return im.resize((300, 300), Image.ANTIALIAS)
+
+
+def main():
+    
+    st.header("AGE AND GENDER PREDICTION")
+    st.write(
+        "Upload an image find out! (preferably a squared image and containing only a face)"
+    )
+    file = st.file_uploader("Upload Photo")
+    if file is not None:
+        st.image(file, width=300)
+        process_and_predict(file)
+        
+
+if __name__ == "__main__":
+    main()
